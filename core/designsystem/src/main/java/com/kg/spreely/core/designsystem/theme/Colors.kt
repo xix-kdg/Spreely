c8894fd5180523f5c4bbbeccdeb44d9930a44fc2
KG <kevindeveza.guido@gmail.com>
2026-07-14 13:16:02 +0800

    [0] Setup core design systems (color, typography and themes)

---
+++ core/designsystem/src/main/java/com/kg/spreely/core/designsystem/theme/Colors.kt
@@ -0,0 +1,53 @@
+package com.kg.spreely.core.designsystem.theme
+
+import androidx.compose.ui.graphics.Color
+
+// --- LIGHT PALETTE ---
+val PrimaryLight = Color(0xFF0F2D2E)
+val OnPrimaryLight = Color(0xFFFFFFFF)
+val PrimaryContainerLight = Color(0xFFD1E7E8)
+val OnPrimaryContainerLight = Color(0xFF002020)
+
+val SecondaryLight = Color(0xFF4B6364)
+val OnSecondaryLight = Color(0xFFFFFFFF)
+val SecondaryContainerLight = Color(0xFFCEDEE0)
+val OnSecondaryContainerLight = Color(0xFF071F21)
+
+val TertiaryLight = Color(0xFF515D7D)
+val OnTertiaryLight = Color(0xFFFFFFFF)
+val TertiaryContainerLight = Color(0xFFD9E2FF)
+val OnTertiaryContainerLight = Color(0xFF0C1A37)
+
+val ErrorLight = Color(0xFFBA1A1A)
+val OnErrorLight = Color(0xFFFFFFFF)
+
+val SurfaceLight = Color(0xFFF9F9F9)
+val OnSurfaceLight = Color(0xFF191C1C)
+val SurfaceVariantLight = Color(0xFFF3F3F4)
+val OnSurfaceVariantLight = Color(0xFF3F4949)
+val OutlineLight = Color(0xFFDADADA)
+
+// --- DARK PALETTE ---
+val PrimaryDark = Color(0xFF7C4DFF)
+val OnPrimaryDark = Color(0xFFFFFFFF)
+val PrimaryContainerDark = Color(0xFF3B0091)
+val OnPrimaryContainerDark = Color(0xFFE6DEFF)
+
+val SecondaryDark = Color(0xFFB1CBCD)
+val OnSecondaryDark = Color(0xFF1D3436)
+val SecondaryContainerDark = Color(0xFF334B4C)
+val OnSecondaryContainerDark = Color(0xFFCEDEE0)
+
+val TertiaryDark = Color(0xFFB9C6EA)
+val OnTertiaryDark = Color(0xFF23304E)
+val TertiaryContainerDark = Color(0xFF394665)
+val OnTertiaryContainerDark = Color(0xFFD9E2FF)
+
+val ErrorDark = Color(0xFFFFB4AB)
+val OnErrorDark = Color(0xFF690005)
+
+val SurfaceDark = Color(0xFF131316)
+val OnSurfaceDark = Color(0xFFE1E3E3)
+val SurfaceVariantDark = Color(0xFF1B1B1E)
+val OnSurfaceVariantDark = Color(0xFFBFC8C8)
+val OutlineDark = Color(0xFF39393C)
